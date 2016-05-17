c408c322576d350f75d6a6d73f91b4d90258ef69
codyguha <aalo.guha@gmail.com>
2016-05-17 00:35:58 -0700

    added messages

--- facebook_survey2.js
+++ facebook_survey2.js
@@ -32,6 +32,9 @@ controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
 
 controller.on('message_received', function(bot, message) {
     console.log(message.attachments)
+    if (message.attachments.type == 'image') {
+        console.log(message.attachments.payload.url)
+    }
 });
 
 /// GET USER INFO !!!
